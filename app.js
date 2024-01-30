3f401146268de356e8e29d0f3783ec2d5f6bc38d
Fernando Espino <mferna.92@gmail.com>
2024-01-30 11:18:17 -0600

    create folder middleware and move cors function

--- app.js
+++ app.js
@@ -1,30 +1,12 @@
 import express, { json } from 'express'
-import cors from 'cors'
 import { moviesRouter } from './routes/movies.js'
+import { corsMiddleware } from './middleware/cors.js'
 
 const app = express()
 app.disable('x-powered-by')
 
 app.use(json())
-app.use(cors({
-	origin: (origin, callback) => {
-		const ACCEPTED_ORIGINS = [
-			'http://localhost:1234',
-            'http://localhost:8080',
-            'http://movies.com',
-		]
-		// validamos si esta en los permitidos
-		if (ACCEPTED_ORIGINS.includes(origin)) {
-        return callback(null, true)
-        }
-        // o si no lo obtenemos (asumiendo que es el mismo dominio)
-        if (!origin) {
-            return callback(null, true)
-        }
-        // si no devolvemos que no tiene accesos
-        return callback(new Error('Not allowed by CORS'))
-    }
-}))
+app.use(corsMiddleware)
 
 app.use('/movies', moviesRouter)
 
