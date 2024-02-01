c831364da8d508e2020bf16793da1c4ef9c20e75
Fernando Espino <mferna.92@gmail.com>
2024-02-01 10:48:40 -0600

    use mysql model in the controller

--- controllers/movies.js
+++ controllers/movies.js
@@ -6,7 +6,6 @@ export class MovieController{
     static async getAll(req, res){
         const { genre } = req.query
         const movies = await MovieModel.getAll({genre})
-
         res.json(movies)
     }
 
