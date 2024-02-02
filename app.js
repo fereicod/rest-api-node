f7aca941cdb91087a2216a2b6348c95b431df366
Fernando Espino <mferna.92@gmail.com>
2024-02-02 00:06:38 -0600

    apply dependency injection

--- app.js
+++ app.js
@@ -1,17 +1,19 @@
 import express, { json } from 'express'
 import { corsMiddleware } from './middlewares/cors.js'
-import { moviesRouter } from './routes/movies.js'
+import { createMovieRouter } from './routes/movies.js'
 
-const app = express()
-app.disable('x-powered-by')
+export const createApp = ({ movieModel }) => {
+    const app = express()
+    app.disable('x-powered-by')
 
-app.use(json())
-app.use(corsMiddleware())
+    app.use(json())
+    app.use(corsMiddleware())
 
-app.use('/movies', moviesRouter)
+    app.use('/movies', createMovieRouter({ movieModel }))
 
-const PORT = process.env.PORT ?? 1234
+    const PORT = process.env.PORT ?? 1234
 
-app.listen(PORT, () => {
-    console.log(`server listening on port http://localhost:${PORT}`)
-})
+    app.listen(PORT, () => {
+        console.log(`server listening on port http://localhost:${PORT}`)
+    })
+}
